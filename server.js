7b45186e0d1820615e2d89c63ec1300afb58818c
habibulloh333 <levi041005@gmail.com>
2025-12-04 17:49:04 +0700

    merubah syntax

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const { authenticateToken, authorizeRole } = require("./auth/auth");
+const { authenticateToken, authorizeRole } = require("./auth");
 const jwt = require("jsonwebtoken");
 
 const app = express();
